daa7a146a6db8b6fae73802ea5c2becc21a5764f
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2011-06-05 22:45:59 +0000

    Change include path.

--- trunk/config/ArmadilloConfig.cmake
+++ trunk/config/ArmadilloConfig.cmake
@@ -1,5 +1,5 @@
 # Where to find Armadillo include files?
-set(ARMADILLO_INCLUDE_DIRS "/home/md/erkale/source/erkale/armadillo-1.1.92/include")
+set(ARMADILLO_INCLUDE_DIRS "/home/md/erkale/armadillo-1.1.92/include")
 # Directory where Armadillo is located
 #set(ARMADILLO_LIBRARY_DIRS "/usr/local/lib64")
 # Libraries to link against
