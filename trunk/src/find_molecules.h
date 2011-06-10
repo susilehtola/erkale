421ed4d18e524b75948f6ec7928b00c9360a4a26
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2011-06-10 14:34:28 +0000

    Specify units.

--- trunk/src/find_molecules.h
+++ trunk/src/find_molecules.h
@@ -8,9 +8,9 @@
 /// Find probable molecules in input
 std::vector< std::vector<size_t> > find_molecules(const std::vector<atom_t> & atoms);
 
-/// Check if atoms are probably bonded together
+/// Check if atoms are probably bonded together, units are Ångström
 bool check_bonds(double d, const std::string & atom1, const std::string & atom2, double tol=0.3);
-/// Check pair for bond
+/// Check pair for bond, units are Ångström
 bool check_bond(const std::string & atom1, const std::string & atom2, double d, const std::string & test1, const std::string & test2, double maxlen);
 
 #endif
