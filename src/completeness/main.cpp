906864f53d4f2f35f43591e9bdaeaf93011ad8bd
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2011-08-26 16:24:00 +0000

    Added missing license boilerplates.

--- src/completeness/main.cpp
+++ src/completeness/main.cpp
@@ -1,3 +1,19 @@
+/*
+ *                This source code is part of
+ * 
+ *                     E  R  K  A  L  E
+ *                             -
+ *                       HF/DFT from Hel
+ *
+ * Written by Jussi Lehtola, 2010-2011
+ * Copyright (c) 2010-2011, Jussi Lehtola
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
 #include <cstdio>
 #include "optimize_completeness.h"
 #include "../basislibrary.h"
