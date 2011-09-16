2b625cf6d0fb5ed984c10cc16fbd62a3c76182f4
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2011-09-16 18:37:25 +0000

    stringutil.{h,cpp}: added parse() and parse_range().

--- trunk/src/stringutil.h
+++ trunk/src/stringutil.h
@@ -26,36 +26,43 @@
 #include <string>
 #include <istream>
 
-// Convert string to lower or upper case
+/// Convert string to lower case
 std::string tolower(const std::string & in);
+/// Convert string to upper case
 std::string toupper(const std::string & in);
 
-// Case insensitive comparison of two strings
+/// Case insensitive comparison of two strings
 int stricmp(const std::string & str1, const std::string & str2);
 
-// Read line from input (skip empty lines)
+/// Read line from input (skip empty lines)
 std::string readline(std::istream & in);
 
-// Split line into words
+/// Split line into words
 std::vector<std::string> splitline(std::string line);
 
-// Read integer
+/// Read integer
 int readint(std::string num);
 
-// Read a number in double precision
+/// Read a number in double precision
 double readdouble(std::string num);
 
-// Print the energies wrt occupancies
+/// Print the energies wrt occupancies
 void print_E(const arma::vec & E, const std::vector<double> & occ);
 
-// Convert memory requirement to readable text
+/// Convert memory requirement to readable text
 std::string memory_size(size_t size);
 
-// Pretty-print symmetric matrix
+/// Pretty-print symmetric matrix
 void print_sym(const arma::mat &mat, bool floatformat=0, double cutoff=1e-3);
 
-// Print orbital energies and coefficients
+/// Print orbital energies and coefficients
 void print_orb(const arma::mat & C, const arma::vec & E);
+
+/// Parse string into pieces separated with a character in the separator
+std::vector<std::string> parse(std::string in, const std::string & separator);
+
+/// Parse input for a range of indices, e.g. 0,2-5,9,11-15,20
+std::vector<size_t> parse_range(const std::string & in);
 
 
 #endif
