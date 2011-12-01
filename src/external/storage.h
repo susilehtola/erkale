08b57b391f8b6101efaf11c1cfdcd22075eeb131
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2011-12-01 16:17:09 +0000

    Added tool for reading Gaussian checkpoint files.

---
+++ src/external/storage.h
@@ -0,0 +1,105 @@
+/*
+ *                This source code is part of
+ *
+ *                     E  R  K  A  L  E
+ *                             -
+ *                       HF/DFT from Hel
+ *
+ * Written by Jussi Lehtola, 2010-2011
+ * Copyright (c) 2010-2011, Jussi Lehtola
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
+#ifndef ERKALE_STORAGE
+#define ERKALE_STORAGE
+
+#include "global.h"
+
+#include <string>
+#include <vector>
+
+/// Double precision number
+typedef struct {
+  /// Name of entry
+  std::string name;
+  /// Value
+  double val;
+} double_st_t;
+
+/// Array of doubles
+typedef struct {
+  /// Name of entry
+  std::string name;
+  /// Value
+  std::vector<double> val;
+} double_vec_st_t;
+
+/// Integer value
+typedef struct {
+  /// Name of entry
+  std::string name;
+  /// Value
+  int val;
+} int_st_t;
+
+/// Array of integers
+typedef struct {
+  /// Name of entry
+  std::string name;
+  /// Value
+  std::vector<int> val;
+} int_vec_st_t;
+
+/// Class for storing input data
+class Storage {
+  /// Stack of integers
+  std::vector<int_st_t> ints;
+  /// Stack of doubles
+  std::vector<double_st_t> doubles;
+  /// Stack of integer arrays
+  std::vector<int_vec_st_t> intvec;
+  /// Stack of double precision arrays
+  std::vector<double_vec_st_t> doublevec;
+  
+ public:
+  /// Constructor
+  Storage();
+  /// Destructor
+  ~Storage();
+
+  /// Add integer value
+  void add(const int_st_t & val);
+  /// Add double precision value
+  void add(const double_st_t & val);
+  /// Add integer vector
+  void add(const int_vec_st_t & val);
+  /// Add double precision vector
+  void add(const double_vec_st_t & val);
+
+  /// Get integer value
+  int get_int(const std::string & name) const;
+  /// Get double precision value
+  double get_double(const std::string & name) const;
+  /// Get integer vector
+  std::vector<int> get_int_vec(const std::string & name) const;
+  /// Get double precision vector
+  std::vector<double> get_double_vec(const std::string & name) const;
+
+  /// Get possible integer keywords
+  std::vector<std::string> find_int(const std::string & name) const;
+  /// Get possible double keywords
+  std::vector<std::string> find_double(const std::string & name) const;
+  /// Get possible integer vector keywords
+  std::vector<std::string> find_int_vec(const std::string & name) const;
+  /// Get possible double vector keywords
+  std::vector<std::string> find_double_vec(const std::string & name) const;
+
+  /// Print contents (vector values, too?)
+  void print(bool vals=false) const;
+};
+
+#endif
