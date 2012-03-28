7d9b896a051154418c0ed01570a3f667c321426c
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2012-03-28 09:09:01 +0000

    Added composition option to bastool.

--- trunk/src/basistool/main.cpp
+++ trunk/src/basistool/main.cpp
@@ -18,7 +18,7 @@
 #include "../stringutil.h"
 #include "../completeness/completeness_profile.h"
 
-std::string cmds[]={"completeness", "decontract", "dump"};
+std::string cmds[]={"completeness", "composition", "decontract", "dump"};
 
 
 void help() {
@@ -71,6 +71,42 @@ int main(int argc, char **argv) {
       fprintf(out,"\n");
     }
     fclose(out);
+
+  } else if(stricmp(cmd,"composition")==0) {
+    // Determine composition of basis set.
+
+    if(argc!=4) {
+      printf("\nUsage: %s input.gbs composition El\n",argv[0]);
+      return 1;
+    }
+
+    // Get element
+    std::string el(argv[3]);
+    // Get the basis set
+    ElementBasisSet elbas=bas.get_element(el);
+
+    // Decontracted basis
+    ElementBasisSet eldec(elbas);
+    eldec.decontract();
+
+    // Get the shells
+    std::vector<FunctionShell> sh=elbas.get_shells();
+    std::vector<FunctionShell> decsh=eldec.get_shells();
+
+    // Count the shells
+    arma::imat Nsh(max_am,2);
+    Nsh.zeros();
+    for(size_t ish=0;ish<decsh.size();ish++)
+      Nsh(decsh[ish].get_am(),0)++;
+    for(size_t ish=0;ish<sh.size();ish++)
+      Nsh(sh[ish].get_am(),1)++;
+
+    // Print the composition
+    printf("\nComposition of basis set is:\n");
+    printf("\tam Nprim Ncontr\n");
+    for(int am=0;am<max_am;am++)
+      if(Nsh(am,0)>0)
+	printf("\t%2c %5i %6i\n",shell_types[am],Nsh(am,0),Nsh(am,1));
 
   } else if(stricmp(cmd,"decontract")==0) {
     // Decontract basis set.
