a8cef3a6072511cd1898174031c012c0e8ba778a
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2013-01-24 19:04:50 +0000

    Added missing license boilerplates.

--- src/eriworker_gentransform.cpp
+++ src/eriworker_gentransform.cpp
@@ -1,3 +1,19 @@
+/*
+ *                This source code is part of
+ * 
+ *                     E  R  K  A  L  E
+ *                             -
+ *                       DFT from Hel
+ *
+ * Written by Susi Lehtola, 2010-2013
+ * Copyright (c) 2010-2013, Susi Lehtola
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
 #include "solidharmonics.h"
 
 /// Maximum angular momentum to do with hardcoded routine
