0a3ebae928786f34580a9cc08c6aaffe97352a1a
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2013-03-06 09:32:02 +0000

    Do conventional calculation on initial guess.

--- trunk/src/guess.cpp
+++ trunk/src/guess.cpp
@@ -43,6 +43,7 @@ void atomic_guess(const BasisSet & basis, arma::mat & C, arma::mat & E, Settings
   set.set_bool("DensityFitting",false);
   set.set_bool("ForcePol",true);
   set.set_bool("Verbose",false);
+  set.set_bool("Direct",false);
 
   // Relax convergence requirements - open shell atoms may be hard to
   // converge
