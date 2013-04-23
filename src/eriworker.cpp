45072535d834b623b33c2e2e9e4ea30cddcd1d65
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2013-04-23 16:54:13 +0000

    Work towards ERI derivatives.

--- src/eriworker.cpp
+++ src/eriworker.cpp
@@ -18,22 +18,13 @@
 #include "linalg.h"
 #include "mathf.h"
 
-ERIWorker::ERIWorker(int maxam, int maxcontr) {
-  if(maxam>=LIBINT_MAX_AM) {
-    ERROR_INFO();
-    throw std::domain_error("You need a version of LIBINT that supports larger angular momentum.\n");
-  }
-
-  // Initialize evaluator
-  init_libint(&libint,maxam,pow(maxcontr,4));
+IntegralWorker::IntegralWorker() {
 }
 
-ERIWorker::~ERIWorker() {
-  free_libint(&libint);
+IntegralWorker::~IntegralWorker() {
 }
 
-
-void ERIWorker::spherical_transform(const GaussianShell *is, const GaussianShell *js, const GaussianShell *ks, const GaussianShell *ls) {
+void IntegralWorker::spherical_transform(const GaussianShell *is, const GaussianShell *js, const GaussianShell *ks, const GaussianShell *ls) {
   const bool is_lm=is->lm_in_use();
   const bool js_lm=js->lm_in_use();
   const bool ks_lm=ks->lm_in_use();
@@ -61,6 +52,37 @@ void ERIWorker::spherical_transform(const GaussianShell *is, const GaussianShell
     transform_j(am_j,Ni_cart,Nk_tgt,Nl_tgt);
   if(is_lm)
     transform_i(am_i,Nj_tgt,Nk_tgt,Nl_tgt);
+}
+
+ERIWorker::ERIWorker(int maxam, int maxcontr) {
+  if(maxam>=LIBINT_MAX_AM) {
+    ERROR_INFO();
+    throw std::domain_error("You need a version of LIBINT that supports larger angular momentum.\n");
+  }
+
+  // Initialize evaluator
+  init_libint(&libint,maxam,pow(maxcontr,4));
+}
+
+ERIWorker::~ERIWorker() {
+  free_libint(&libint);
+}
+
+dERIWorker::dERIWorker(int maxam, int maxcontr) {
+  if(maxam>=LIBDERIV_MAX_AM1) {
+    ERROR_INFO();
+    throw std::domain_error("You need a version of LIBDERIV that supports larger angular momentum.\n");
+  }
+
+  // Maximum amount of basis functions is
+  int nbf=(maxam+1)*(maxam+2)/2;
+
+  // Initialize evaluator
+  init_libderiv1(&libderiv,maxam,pow(maxcontr,4),pow(nbf,4));
+}
+
+dERIWorker::~dERIWorker() {
+  free_libderiv(&libderiv);
 }
 
 void ERIWorker::compute_cartesian(const GaussianShell *is, const GaussianShell *js, const GaussianShell *ks, const GaussianShell *ls) {
@@ -97,7 +119,7 @@ void ERIWorker::compute_cartesian(const GaussianShell *is, const GaussianShell *
   }
 
   // Compute data for LIBINT
-  compute_libint_data(libint,ip,jp,mmax);
+  compute_libint_data(ip,jp,mmax);
 
   // Pointer to integrals table
   double *ints;
@@ -157,7 +179,106 @@ void ERIWorker::compute_cartesian(const GaussianShell *is, const GaussianShell *
   }
 }
 
-eri_precursor_t compute_precursor(const GaussianShell *is, const GaussianShell *js) {
+void dERIWorker::compute_cartesian() {
+  eri_precursor_t ip=compute_precursor(is,js);
+  eri_precursor_t jp=compute_precursor(ks,ls);
+
+  // Compute shell of cartesian ERI derivatives using libderiv
+
+  // Libint computes (ab|cd) for
+  // l(a)>=l(b), l(c)>=l(d) and l(a)+l(b)>=l(c)+l(d)
+  // where l(a) is the angular momentum type of the a shell.
+  // THIS ROUTINE ASSUMES THAT THE SHELLS ARE ALREADY IN CORRECT ORDER!
+
+  // The sum of angular momenta is
+  int mmax=is->get_am()+js->get_am()+ks->get_am()+ls->get_am();
+
+  // Figure out the number of contractions
+  size_t Ncomb=is->get_Ncontr()*js->get_Ncontr()*ks->get_Ncontr()*ls->get_Ncontr();
+
+  // Check that all is in order
+  if(is->get_am()<js->get_am()) {
+    ERROR_INFO();
+    throw std::runtime_error("lambda_i < lambda_j\n");
+  }
+
+  if(ks->get_am()<ls->get_am()) {
+    ERROR_INFO();
+    throw std::runtime_error("lambda_k < lambda_l\n");
+  }
+
+  if( (is->get_am()+js->get_am()) > (ks->get_am()+ls->get_am())) {
+    ERROR_INFO();
+    throw std::runtime_error("lambda_k + lambda_l < lambda_i + lambda_j\n");
+  }
+
+  // Compute data for LIBINT
+  compute_libderiv_data(ip,jp,mmax);
+
+  // Now we can compute the integrals using libint:
+  build_deriv1_eri[is->get_am()][js->get_am()][ks->get_am()][ls->get_am()](&libderiv,Ncomb);
+
+  // and plug in the normalization factors.
+  size_t ind_i, ind_ij, ind_ijk, ind;
+  double norm_i, norm_ij, norm_ijk, norm;
+  
+  // Numbers of functions on each shell
+  std::vector<shellf_t> ci=is->get_cart();
+  std::vector<shellf_t> cj=js->get_cart();
+  std::vector<shellf_t> ck=ks->get_cart();
+  std::vector<shellf_t> cl=ls->get_cart();
+  
+  // Integrals computed by libderiv
+  const int idx[]={0, 1, 2, 6, 7, 8, 9, 10, 11};
+
+  for(size_t ii=0;ii<ci.size();ii++) {
+    ind_i=ii*cj.size();
+    norm_i=ci[ii].relnorm;
+    for(size_t ji=0;ji<cj.size();ji++) {
+      ind_ij=(ind_i+ji)*ck.size();
+      norm_ij=cj[ji].relnorm*norm_i;
+      for(size_t ki=0;ki<ck.size();ki++) {
+	  ind_ijk=(ind_ij+ki)*cl.size();
+	  norm_ijk=ck[ki].relnorm*norm_ij;
+	  for(size_t li=0;li<cl.size();li++) {
+	    // Index in computed integrals table
+	    ind=ind_ijk+li;
+	    // Total norm factor
+	    norm=cl[li].relnorm*norm_ijk;
+	    // Normalize integrals
+	    for(size_t iidx=0;iidx<sizeof(idx)/sizeof(idx[0]);iidx++)
+	      libderiv.ABCD[idx[iidx]][ind]*=norm;
+	  }
+      }
+    }
+  }
+}
+
+
+void dERIWorker::get(int idx, std::vector<double> & ints) {
+  // First, copy data. Amount of integrals
+  size_t N=(is->get_Ncart())*(js->get_Ncart())*(ks->get_Ncart())*(ls->get_Ncart());
+  input.resize(N);
+
+  if((idx>=0 && idx<3) || (idx>5 && idx<12)) {
+    // Integrals have been computed explicitely.
+    for(size_t i=0;i<N;i++)
+      input[i]=libderiv.ABCD[idx][i];
+  } else {
+    // Derivate wrt B_i: d/dB_i = - d/dA_i - d/dC_i - d/dD_i
+    for(size_t i=0;i<N;i++)
+      input[i]= -libderiv.ABCD[idx-3][i] - libderiv.ABCD[idx+3][i] - libderiv.ABCD[idx+6][i];
+  }
+  
+  // Reorder integrals
+  reorder(is,js,ks,ls,swap_ij,swap_kl,swap_ijkl);
+  // and transform them into the spherical basis
+  spherical_transform(is_orig,js_orig,ks_orig,ls_orig);
+  // and return them
+  ints=input;
+}
+
+eri_precursor_t IntegralWorker::compute_precursor(const GaussianShell *is, const GaussianShell *js) {
   // Returned precursor
   eri_precursor_t r;
 
@@ -221,7 +342,7 @@ eri_precursor_t compute_precursor(const GaussianShell *is, const GaussianShell *
   return r;
 }
 
-void compute_libint_data(Libint_t & libint, const eri_precursor_t & ip, const eri_precursor_t &jp, int mmax) {
+void ERIWorker::compute_libint_data(const eri_precursor_t & ip, const eri_precursor_t &jp, int mmax) {
   // Store AB and CD
   for(int i=0;i<3;i++) {
     libint.AB[i]=ip.AB(i);
@@ -293,6 +414,85 @@ void compute_libint_data(Libint_t & libint, const eri_precursor_t & ip, const er
     }
 }
 
+void dERIWorker::compute_libderiv_data(const eri_precursor_t & ip, const eri_precursor_t &jp, int mmax) {
+  // Store AB and CD
+  for(int i=0;i<3;i++) {
+    libderiv.AB[i]=ip.AB(i);
+    libderiv.CD[i]=jp.AB(i);
+  }
+
+  size_t ind=0;
+
+  // Compute primitive data
+  for(size_t p=0;p<ip.ic.size();p++)
+    for(size_t q=0;q<ip.jc.size();q++) {
+      double zeta=ip.zeta(p,q);
+
+      // Compute overlaps for auxiliary integrals
+      double S12=ip.S(p,q);
+
+      for(size_t r=0;r<jp.ic.size();r++)
+	for(size_t s=0;s<jp.jc.size();s++) {
+	  double eta=jp.zeta(r,s);
+
+	  // Overlap for auxiliary integral
+	  double S34=jp.S(r,s);
+
+	  // Reduced exponent
+          double rho=zeta*eta/(zeta+eta);
+
+	  // Four-product center
+	  arma::vec W=(zeta*slicevec(ip.P,p,q)+eta*slicevec(jp.P,r,s))/(zeta+eta);
+
+	  arma::vec PQ=slicevec(ip.P,p,q)-slicevec(jp.P,r,s);
+	  arma::vec WP=W-slicevec(ip.P,p,q);
+	  arma::vec WQ=W-slicevec(jp.P,r,s);
+
+	  double rpqsq=arma::dot(PQ,PQ);
+
+	  // Helper variable
+	  prim_data data;
+
+          // Compute and store PA, QC, WP and WQ
+          for(int i=0;i<3;i++) {
+            data.U[0][i]=ip.PA(p,q,i); // PA
+	    data.U[1][i]=ip.PB(p,q,i); // PB
+            data.U[2][i]=jp.PA(r,s,i); // QC
+	    data.U[3][i]=jp.PB(r,s,i); // QD
+            data.U[4][i]=WP(i); // WP
+            data.U[5][i]=WQ(i); // WQ
+          }
+
+	  // Store exponents
+	  data.twozeta_a=2.0*ip.ic[p].z;
+	  data.twozeta_b=2.0*ip.jc[q].z;
+	  data.twozeta_c=2.0*jp.ic[r].z;
+	  data.twozeta_d=2.0*jp.jc[s].z;
+
+	  data.oo2z=0.5/zeta;
+	  data.oo2n=0.5/eta;
+	  data.oo2zn=0.5/(eta+zeta);
+	  data.oo2p=0.5/rho;
+	  data.poz=rho/zeta;
+	  data.pon=rho/eta;
+
+	  // Prefactor of Boys' function is
+	  double prefac=2.0*sqrt(rho/M_PI)*S12*S34;
+	  // and its argument is
+	  double boysarg=rho*rpqsq;
+	  // Evaluate Boys' function
+	  std::vector<double> bf=boysF_arr(mmax+1,boysarg);
+
+	  // Store auxiliary integrals
+	  for(int i=0;i<=mmax+1;i++)
+	    data.F[i]=prefac*bf[i];
+
+	  // We have all necessary data; store quartet.
+	  libderiv.PrimQuartet[ind++]=data;
+	}
+    }
+}
+
 void ERIWorker::compute(const GaussianShell *is_orig, const GaussianShell *js_orig, const GaussianShell *ks_orig, const GaussianShell *ls_orig, std::vector<double> & ints) {
   // Calculate ERIs and transform them to spherical harmonics basis, if necessary.
 
@@ -324,15 +524,63 @@ void ERIWorker::compute(const GaussianShell *is_orig, const GaussianShell *js_or
     std::swap(js,ls);
   }
 
-  // Get the cartesian ERIs:
+  // Get the cartesian ERIs
   compute_cartesian(is,js,ks,ls);
+  // Reorder them
+  reorder(is,js,ks,ls,swap_ij,swap_kl,swap_ijkl);
+  // and transform them into the spherical basis
+  spherical_transform(is_orig,js_orig,ks_orig,ls_orig);
 
+  // Finally, return the integrals
+  ints=input;
+}
+
+void dERIWorker::compute(const GaussianShell *is_origv, const GaussianShell *js_origv, const GaussianShell *ks_origv, const GaussianShell *ls_origv) {
+  // Calculate derivative ERIs
+
+  is_orig=is_origv;
+  js_orig=js_origv;
+  ks_orig=ks_origv;
+  ls_orig=ls_origv;
+
+  is=is_orig;
+  js=js_orig;
+  ks=ks_orig;
+  ls=ls_orig;
+
+  // Did we need to swap the indices?
+  swap_ij=false;
+  swap_kl=false;
+  swap_ijkl=false;
+
+  // Check order and swap shells if necessary
+  if(is->get_am()<js->get_am()) {
+    swap_ij=true;
+    std::swap(is,js);
+  }
+
+  if(ks->get_am()<ls->get_am()) {
+    swap_kl=true;
+    std::swap(ks,ls);
+  }
+
+  if(is->get_am()+js->get_am() > ks->get_am() + ls->get_am()) {
+    swap_ijkl=true;
+    std::swap(is,ks);
+    std::swap(js,ls);
+  }
+
+  // Get the cartesian ERIs
+  compute_cartesian();
+}
+
+void IntegralWorker::reorder(const GaussianShell *is, const GaussianShell *js, const GaussianShell *ks, const GaussianShell *ls, bool swap_ij, bool swap_kl, bool swap_ijkl) {
   // Integrals are now in the input array.
   // Get them in the original order
   if(swap_ij || swap_kl || swap_ijkl) {
     // Check that we have enough memory
     output.resize(input.size());
-
+    
     // Indices
     size_t ind_i, ind_ij, ind_ijk, ind, indout;
     
@@ -356,19 +604,13 @@ void ERIWorker::compute(const GaussianShell *is_orig, const GaussianShell *js_or
 	}
       }
     }
-
+    
     // Swap arrays
     std::swap(input,output);
-  }
-  
-  // and transform them into the spherical basis
-  spherical_transform(is_orig,js_orig,ks_orig,ls_orig);
-
-  // Return integrals
-  ints=input;
+  }  
 }
 
-size_t get_swapped_ind(size_t i, size_t Ni, size_t j, size_t Nj, size_t k, size_t Nk, size_t l, size_t Nl, bool swap_ij, bool swap_kl, bool swap_ijkl) {
+size_t IntegralWorker::get_swapped_ind(size_t i, size_t Ni, size_t j, size_t Nj, size_t k, size_t Nk, size_t l, size_t Nl, bool swap_ij, bool swap_kl, bool swap_ijkl) {
   // Compute indices of swapped integrals.
 
   // First, swap ij-kl if necessary.
