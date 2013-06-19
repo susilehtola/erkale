a11f909692ce0ee724e5684b4cfcbefe747ba3e7
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2013-06-19 12:40:01 +0000

    More work into unitary optimization.

--- src/unitary.h
+++ src/unitary.h
@@ -3,11 +3,14 @@
 
 #include "global.h"
 #include "basis.h"
+#include "scf.h"
 #include <armadillo>
 
 enum unitmethod {
-  /// Polynomial search
-  POLYNOMIAL,
+  /// Polynomial search, fit derivative
+  POLY_DF,
+  /// Polynomial search, fit function and derivative
+  POLY_FDF,
   /// Armijo method
   ARMIJO
 };
@@ -25,14 +28,13 @@ class Unitary {
   /// Convergence threshold
   double eps;
 
-  /* Polynomial search */
-  /// Amount of points to use
+  /// Amount of points to use in polynomial search
   int npoly;
-  /// Fit function and derivative instead of just derivative?
-  bool fdf;
 
   /// Value of cost function
   double J;
+  /// Old value
+  double oldJ;
   /// G matrices
   std::vector<arma::cx_mat> G;
   /// H matrix
@@ -47,6 +49,31 @@ class Unitary {
 
   /// Check convergence
   virtual bool converged() const;
+  /// Print progress
+  virtual void print_progress(size_t k) const;
+
+  /// Check that the matrix is unitary
+  void check_unitary(const arma::cx_mat & W) const;
+  /// Classify matrix
+  void classify(const arma::cx_mat & W) const;
+
+  /// Get rotation matrix with wanted step size
+  arma::cx_mat get_rotation(double step) const;
+
+  /// Armijo step, return step length
+  double armijo_step(const arma::cx_mat & W);
+  /// Polynomial step (fit only derivative), return step length
+  double polynomial_step_df(const arma::cx_mat & W);
+  /// Polynomial step (fit function and derivative), return step length
+  double polynomial_step_fdf(const arma::cx_mat & W);
+
+ public:
+  /// Constructor
+  Unitary(int q, double thr, bool maximize, bool verbose=true);
+  /// Destructor
+  ~Unitary();
+
+
   /// Evaluate cost function
   virtual double cost_func(const arma::cx_mat & W)=0;
   /// Evaluate derivative of cost function
@@ -54,29 +81,13 @@ class Unitary {
   /// Evaluate cost function and its derivative
   virtual void cost_func_der(const arma::cx_mat & W, double & f, arma::cx_mat & der)=0;
 
-  /// Check that the matrix is unitary
-  void check_unitary(const arma::cx_mat & W) const;
-
-  /// Get rotation matrix with wanted step size
-  arma::cx_mat get_rotation(double step) const;
-
-  /// Armijo step, return step length
-  double armijo_step(arma::cx_mat & W);
-  /// Polynomial step, return step length
-  double polynomial_step(arma::cx_mat & W);
-
- public:
-  /// Constructor
-  Unitary(int q, double thr, bool maximize=true, bool verbose=true);
-  /// Destructor
-  ~Unitary();
-
   /// Set polynomial search options
-  void set_poly(int n, bool fdf);
+  void set_poly(int n);
 
   /// Unitary optimization
-  double optimize(arma::cx_mat & W, enum unitmethod met=POLYNOMIAL);
+  double optimize(arma::cx_mat & W, enum unitmethod met=POLY_DF);
 };
+
 
 /// Boys localization
 class Boys : public Unitary {
@@ -89,6 +100,7 @@ class Boys : public Unitary {
   /// r_z matrix
   arma::mat rz;
 
+
  public:
   Boys(const BasisSet & basis, const arma::mat & C, double thr, bool verbose=true);
   ~Boys();
@@ -100,5 +112,62 @@ class Boys : public Unitary {
   /// Evaluate cost function and its derivative
   void cost_func_der(const arma::cx_mat & W, double & f, arma::cx_mat & der);
 };
+
+
+/// Perdew-Zunger self-interaction correction
+class PZSIC : public Unitary {
+  /// SCF object for constructing Fock matrix
+  SCF * solver;
+  /// Settings for DFT calculation
+  dft_t dft;
+  /// XC grid
+  DFTGrid * grid;
+
+  /// Solution
+  rscf_t sol;
+  /// Occupation number
+  double occnum;
+  /// Coefficient for PZ-SIC
+  double pzcor;
+
+  /// Orbital Fock matrices
+  std::vector<arma::mat> Forb;
+  /// Orbital SIC energies
+  arma::vec Eorb;
+  /// Kappa matrix
+  arma::cx_mat kappa;
+
+  /// SIC Fock operator
+  arma::mat HSIC;
+
+  /// Check convergence
+  bool converged();
+
+  /// Calculate R and K
+  void get_rk(double & R, double & K) const;
+
+  /// Print progress
+  void print_progress(size_t k) const;
+  /// Check convergence
+  bool converged() const;
+
+ public:
+  /// Constructor
+  PZSIC(SCF *solver, dft_t dft, DFTGrid * grid, bool verbose);
+  /// Destructor
+  ~PZSIC();
+
+  /// Set orbitals
+  void set(const rscf_t & ref, double occ, double pzcor);
+
+  /// Evaluate cost function
+  double cost_func(const arma::cx_mat & W);
+  /// Evaluate derivative of cost function
+  arma::cx_mat cost_der(const arma::cx_mat & W);
+  /// Evaluate cost function and its derivative
+  void cost_func_der(const arma::cx_mat & W, double & f, arma::cx_mat & der);
+};
+
+
 
 #endif
