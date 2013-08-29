0b678c322b74f6927478bd4b8347f7b7cc8acf99
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2013-08-29 15:23:41 +0000

    Tabulate Lebedev rule orders and amount of points.

--- trunk/src/lebedev.h
+++ trunk/src/lebedev.h
@@ -22,8 +22,8 @@
 #include <vector>
 
 /// Orders of supported rules (integrates spherical harmonics exactly up to given order)
-const int lebedev_orders[]={3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 35, 41, 47, 53, 59, 65, 71, 77, 83, 89, 95, 101, 107, 113, 119, 125, 131};/// The number of points used by the supported rules
-const int lebedev_degrees[]={6, 14, 26, 38, 50, 74, 86, 110, 146, 170, 194, 230, 266,302, 350, 434, 590, 770, 974, 1202, 1454, 1730, 2030, 2354, 2702, 3074, 3470, 3890, 4334, 4802, 5294, 5810};
+const int lebedev_orders[] ={3,  5,  7,  9, 11, 13, 15,  17,  19,  21,  23,  25,  27,  29,  31,  35,  41,  47,  53,   59,   65,   71,   77,   83,   89,   95,  101,  107,  113,  119,  125,  131};/// The number of points used by the supported rules
+const int lebedev_degrees[]={6, 14, 26, 38, 50, 74, 86, 110, 146, 170, 194, 230, 266, 302, 350, 434, 590, 770, 974, 1202, 1454, 1730, 2030, 2354, 2702, 3074, 3470, 3890, 4334, 4802, 5294, 5810};
 
 /// Coordinates and weight for a point in Lebedev quadrature
 typedef struct {
