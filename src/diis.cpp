0f8496adc20531e0a4dd834212e766c8dde9cefd
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2013-09-30 11:31:20 +0000

    Solved DIIS convergence problems with unrestricted systems.

--- src/diis.cpp
+++ src/diis.cpp
@@ -24,7 +24,11 @@
 // Maximum allowed absolute weight for a Fockian.
 #define MAXWEIGHT 10.0
 
-bool operator<(const diis_entry_t & lhs, const diis_entry_t & rhs) {
+bool operator<(const diis_pol_entry_t & lhs, const diis_pol_entry_t & rhs) {
+  return lhs.E < rhs.E;
+}
+
+bool operator<(const diis_unpol_entry_t & lhs, const diis_unpol_entry_t & rhs) {
   return lhs.E < rhs.E;
 }
 
@@ -42,13 +46,17 @@ DIIS::~DIIS() {
 }
 
 void DIIS::clear() {
-  stack.clear();
+  pol.clear();
+  unpol.clear();
 }
 
-
 void DIIS::update(const arma::mat & F, const arma::mat & P, double E, double & error) {
+  if(pol.size()) {
+    throw std::runtime_error("Trying to add spin-unpolarized entry to spin-polarized DIIS stack!\n");
+  }
+
   // New entry
-  diis_entry_t hlp;
+  diis_unpol_entry_t hlp;
   hlp.F=F;
   hlp.P=P;
   hlp.E=E;
@@ -56,7 +64,7 @@ void DIIS::update(const arma::mat & F, const arma::mat & P, double E, double & e
   // Compute error matrix
   arma::mat errmat=F*P*S-S*P*F;
   // and transform it to the orthonormal basis (1982 paper, page 557)
-  errmat=Sinvh*errmat*Sinvh;
+  errmat=arma::trans(Sinvh)*errmat*Sinvh;
   // and store it
   hlp.err=MatToVec(errmat);
 
@@ -64,17 +72,64 @@ void DIIS::update(const arma::mat & F, const arma::mat & P, double E, double & e
   error=max_abs(errmat);
 
   // Is stack full?
-  if(stack.size()==imax) {
-    stack.erase(stack.begin()+stack.size()-1);
+  if(unpol.size()==imax) {
+    unpol.erase(unpol.begin()+unpol.size()-1);
   }
   // Add to stack and resort
-  stack.push_back(hlp);
-  std::stable_sort(stack.begin(),stack.end());
+  unpol.push_back(hlp);
+  std::stable_sort(unpol.begin(),unpol.end());
+}
+
+void DIIS::update(const arma::mat & Fa, const arma::mat & Fb, const arma::mat & Pa, const arma::mat & Pb, double E, double & error) {
+  if(unpol.size()) {
+    throw std::runtime_error("Trying to add spin-polarized entry to spin-unpolarized DIIS stack!\n");
+  }
+
+  // New entry
+  diis_pol_entry_t hlp;
+  hlp.Fa=Fa;
+  hlp.Fb=Fb;
+  hlp.Pa=Pa;
+  hlp.Pb=Pb;
+  hlp.E=E;
+
+  // Compute error matrices
+  arma::mat errmata=Fa*Pa*S-S*Pa*Fa;
+  arma::mat errmatb=Fb*Pb*S-S*Pb*Fb;
+  // and transform them to the orthonormal basis (1982 paper, page 557)
+  arma::mat errmat=arma::trans(Sinvh)*(errmata+errmatb)*Sinvh;
+  // and store it
+  hlp.err=MatToVec(errmat);
+
+  // DIIS error is
+  error=max_abs(errmat);
+
+  // Is stack full?
+  if(pol.size()==imax) {
+    pol.erase(pol.begin()+pol.size()-1);
+  }
+  // Add to stack and resort
+  pol.push_back(hlp);
+  std::stable_sort(pol.begin(),pol.end());
 }
 
 arma::vec DIIS::get_weights(bool c1) {
+  // Collect error vectors
+  std::vector<arma::vec> errs;
+  if(unpol.size() && pol.size())
+    throw std::runtime_error("Error - DIIS has both spin-polarized and spin-unpolarized error stacks!\n");
+  if(unpol.size()) {
+    errs.resize(unpol.size());
+    for(size_t i=0;i<unpol.size();i++)
+      errs[i]=unpol[i].err;
+  } else if(pol.size()) {
+    errs.resize(pol.size());
+    for(size_t i=0;i<pol.size();i++)
+      errs[i]=pol[i].err;
+  }
+
   // Size of LA problem
-  int N=(int) stack.size();
+  int N=(int) errs.size();
 
   // DIIS weights
   arma::vec sol(N);
@@ -93,7 +148,7 @@ arma::vec DIIS::get_weights(bool c1) {
     // Compute errors
     for(int i=0;i<N;i++)
       for(int j=0;j<N;j++) {
-	B(i,j)=arma::dot(stack[i].err,stack[j].err);
+	B(i,j)=arma::dot(errs[i],errs[j]);
       }
     // Fill in the rest of B
     for(int i=0;i<N;i++) {
@@ -114,8 +169,11 @@ arma::vec DIIS::get_weights(bool c1) {
       // Check that weights are within tolerance
       for(int i=0;i<N;i++)
 	if(fabs(X(i))>=MAXWEIGHT) {
-	  printf("Large coefficient produced by DIIS. Reducing to %i matrices.\n",(int) stack.size()-1);
-	  stack.erase(stack.begin()+stack.size()-1);
+	  printf("Large coefficient produced by DIIS. Reducing to %i matrices.\n",N-1);
+	  if(pol.size())
+	    pol.erase(pol.begin()+pol.size()-1);
+	  if(unpol.size())
+	    unpol.erase(unpol.begin()+unpol.size()-1);
 	  return get_weights(c1);
 	}
 
@@ -142,7 +200,7 @@ arma::vec DIIS::get_weights(bool c1) {
     // Compute errors
     for(int i=0;i<N;i++)
       for(int j=0;j<N;j++) {
-	B(i,j)=arma::dot(stack[i].err,stack[j].err);
+	B(i,j)=arma::dot(errs[i],errs[j]);
       }
 
     // Solve eigenvectors of B
@@ -160,13 +218,13 @@ arma::vec DIIS::get_weights(bool c1) {
     // Choose solution by picking out solution with smallest error
     std::vector<double> errors(N);
     // Helper array
-    arma::vec werr(stack[0].err.n_elem);
+    arma::vec werr(errs[0].n_elem);
     for(int i=0;i<N;i++) {
       // Zero out helper
       werr.zeros();
       // Compute weighed error
       for(int j=0;j<N;j++)
-	werr+=Q(j,i)*stack[j].err;
+	werr+=Q(j,i)*errs[j];
       // The error is
       errors[i]=arma::dot(werr,werr);
     }
@@ -207,19 +265,57 @@ arma::vec DIIS::get_weights(bool c1) {
 }
 
 void DIIS::solve_F(arma::mat & F, bool c1) {
+  if(!unpol.size())
+    throw std::runtime_error("Trying to get DIIS-averaged spin-unpolarized Fock matrix while no entries exist!\n");
+  
   arma::vec sol=get_weights(c1);
  
   // Form weighted Fock matrix
   F.zeros();
-  for(size_t i=0;i<stack.size();i++)
-    F+=sol(i)*stack[i].F;
+  for(size_t i=0;i<unpol.size();i++)
+    F+=sol(i)*unpol[i].F;
+}
+
+void DIIS::solve_F(arma::mat & Fa, arma::mat & Fb, bool c1) {
+  if(!pol.size())
+    throw std::runtime_error("Trying to get DIIS-averaged spin-polarized Fock matrix while no entries exist!\n");
+  
+  arma::vec sol=get_weights(c1);
+ 
+  // Form weighted Fock matrix
+  Fa.zeros();
+  Fb.zeros();
+  for(size_t i=0;i<pol.size();i++) {
+    Fa+=sol(i)*pol[i].Fa;
+    Fb+=sol(i)*pol[i].Fb;
+  }
 }
 
 void DIIS::solve_P(arma::mat & P, bool c1) {
+  if(!unpol.size())
+    throw std::runtime_error("Trying to get DIIS-averaged spin-unpolarized density matrix while no entries exist!\n");
+  
   arma::vec sol=get_weights(c1);
-
+ 
   // Form weighted density matrix
   P.zeros();
-  for(size_t i=0;i<stack.size();i++)
-    P+=sol(i)*stack[i].P;
+  for(size_t i=0;i<unpol.size();i++)
+    P+=sol(i)*unpol[i].P;
 }
+
+void DIIS::solve_P(arma::mat & Pa, arma::mat & Pb, bool c1) {
+  if(!pol.size())
+    throw std::runtime_error("Trying to get DIIS-averaged spin-polarized density matrix while no entries exist!\n");
+  
+  arma::vec sol=get_weights(c1);
+ 
+  // Form weighted density matrix
+  Pa.zeros();
+  Pb.zeros();
+  for(size_t i=0;i<pol.size();i++) {
+    Pa+=sol(i)*pol[i].Pa;
+    Pb+=sol(i)*pol[i].Pb;
+  }
+}
+
+
