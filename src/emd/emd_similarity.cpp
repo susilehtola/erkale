60d331cc2073ae7c0cb5c1fc6574f00a9cfe8f6c
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2013-11-24 18:37:19 +0000

    Improvements on the EMD similarity calculations.

--- src/emd/emd_similarity.cpp
+++ src/emd/emd_similarity.cpp
@@ -19,10 +19,12 @@ double similarity_quadrature(const std::vector<double> & rad, const std::vector<
       double a_ave=0.0;
       for(size_t iang=0;iang<angmesh.size();iang++)
 	a_ave+=emd_a[ir][iang]*angmesh[iang].w;
+      a_ave/=4.0*M_PI;
       // Average of B
       double b_ave=0.0;
       for(size_t iang=0;iang<angmesh.size();iang++)
 	b_ave+=emd_b[ir][iang]*angmesh[iang].w;
+      b_ave/=4.0*M_PI;
 
       // Increment similarity measure
       sim+=std::pow(rad[ir],2*k+2)*a_ave*b_ave*wrad[ir];
@@ -68,12 +70,31 @@ arma::vec emd_moments(const std::vector<double> & rad, const std::vector<double>
   return mom;
 }
 
-arma::mat emd_similarity(const BasisSet & basis_a, const arma::mat & P_a, const BasisSet & basis_b, const arma::mat & P_b, int nrad, int lmax, bool verbose) {
-  // Get evaluators
-  std::vector< std::vector<size_t> > idents_a, idents_b;
-  std::vector< std::vector<GTO_Fourier> > fourier_a=fourier_expand(basis_a,idents_a);
-  std::vector< std::vector<GTO_Fourier> > fourier_b=fourier_expand(basis_b,idents_b);
+void fill_mesh(const BasisSet & basis, const arma::mat & P, const std::vector<double> & rad, const std::vector<lebedev_point_t> & angmesh, std::vector< std::vector<double> > & emd) {
+  // List of identical functions
+  std::vector< std::vector<size_t> > idents;
+  // Fourier transforms
+  std::vector< std::vector<GTO_Fourier> > fourier=fourier_expand(basis,idents);
+
+  emd.resize(rad.size());
   
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+  for(size_t ir=0;ir<rad.size();ir++) {
+    emd[ir].resize(angmesh.size());
+    for(size_t iang=0;iang<angmesh.size();iang++) {
+      // Coordinate is
+      double px=rad[ir]*angmesh[iang].x;
+      double py=rad[ir]*angmesh[iang].y;
+      double pz=rad[ir]*angmesh[iang].z;
+      
+      emd[ir][iang]=eval_emd(basis, P, fourier, idents, px, py, pz);
+    }
+  }
+}
+
+arma::cube emd_overlap(const BasisSet & basis_a, const arma::mat & P_a, const BasisSet & basis_b, const arma::mat & P_b, int nrad, int lmax, bool verbose) {
   // Get Chebyshev nodes and weights for radial part
   std::vector<double> xc, wc;
   chebyshev(nrad,xc,wc);
@@ -94,13 +115,6 @@ arma::mat emd_similarity(const BasisSet & basis_a, const arma::mat & P_a, const 
   // Get angular quadrature rule
   std::vector<lebedev_point_t> angmesh=lebedev_sphere(lmax);
 
-  // EMDs
-  std::vector< std::vector<double> > emd_a(rad.size()), emd_b(rad.size());
-  for(size_t ir=0;ir<rad.size();ir++) {
-    emd_a[ir].resize(angmesh.size());
-    emd_b[ir].resize(angmesh.size());
-  }
-
   Timer t;
 
   if(verbose) {
@@ -111,18 +125,8 @@ arma::mat emd_similarity(const BasisSet & basis_a, const arma::mat & P_a, const 
   }
 
   // Compute EMDs on mesh
-#ifdef _OPENMP
-#pragma omp parallel for
-#endif
-  for(size_t ir=0;ir<rad.size();ir++)
-    for(size_t iang=0;iang<angmesh.size();iang++) {
-      // Coordinate is
-      double px=rad[ir]*angmesh[iang].x;
-      double py=rad[ir]*angmesh[iang].y;
-      double pz=rad[ir]*angmesh[iang].z;
-
-      emd_a[ir][iang]=eval_emd(basis_a, P_a, fourier_a, idents_a, px, py, pz);
-    }
+  std::vector< std::vector<double> > emd_a;
+  fill_mesh(basis_a,P_a,rad,angmesh,emd_a);
 
   if(verbose) {
     printf("done (%s).\n",t.elapsed().c_str());
@@ -130,19 +134,9 @@ arma::mat emd_similarity(const BasisSet & basis_a, const arma::mat & P_a, const 
     fflush(stdout);
     t.set();
   }
-
-#ifdef _OPENMP
-#pragma omp parallel for
-#endif
-  for(size_t ir=0;ir<rad.size();ir++)
-    for(size_t iang=0;iang<angmesh.size();iang++) {
-      // Coordinate is
-      double px=rad[ir]*angmesh[iang].x;
-      double py=rad[ir]*angmesh[iang].y;
-      double pz=rad[ir]*angmesh[iang].z;
-
-      emd_b[ir][iang]=eval_emd(basis_b, P_b, fourier_b, idents_b, px, py, pz);
-    }
+  
+  std::vector< std::vector<double> > emd_b;
+  fill_mesh(basis_b,P_b,rad,angmesh,emd_b);
 
   if(verbose) {
     printf("done (%s).\n",t.elapsed().c_str());
@@ -152,7 +146,7 @@ arma::mat emd_similarity(const BasisSet & basis_a, const arma::mat & P_a, const 
     arma::vec amom=emd_moments(rad,wrad,angmesh,emd_a);
     arma::vec bmom=emd_moments(rad,wrad,angmesh,emd_b);
 
-    printf("\nMoments of the EMD computed on the grid\n");
+    printf("\nMoments of the radial EMD computed on the grid (for comparison with accurate values)\n");
     printf("%4s\t%9s\t%9s\n","k","A","B");
     for(int k=-2;k<=4;k++)
       printf("%4i\t%e\t%e\n",k,amom(k+2),bmom(k+2));
@@ -162,21 +156,16 @@ arma::mat emd_similarity(const BasisSet & basis_a, const arma::mat & P_a, const 
     t.set();
   }
 
-  
-
   // Get the similarity measures
-  arma::mat ret(4,2);
-  for(int k=-1;k<4;k++) {
-    double AA=similarity_quadrature(rad,wrad,angmesh,emd_a,emd_a,k,false);
-    double AB=similarity_quadrature(rad,wrad,angmesh,emd_a,emd_b,k,false);
-    double BB=similarity_quadrature(rad,wrad,angmesh,emd_b,emd_b,k,false);
-    ret(1+k,0)=sqrt(AA+BB-2*AB);
+  arma::cube ret(4,3,2);
+  for(int k=-1;k<=2;k++)
+    for(int ave=0;ave<=1;ave++) {
+      ret(1+k,0,ave)=similarity_quadrature(rad,wrad,angmesh,emd_a,emd_a,k,ave); // AA
+      ret(1+k,1,ave)=similarity_quadrature(rad,wrad,angmesh,emd_b,emd_b,k,ave); // BB
+      ret(1+k,2,ave)=similarity_quadrature(rad,wrad,angmesh,emd_a,emd_b,k,ave); // AB
 
-    double AAa=similarity_quadrature(rad,wrad,angmesh,emd_a,emd_a,k,true);
-    double ABa=similarity_quadrature(rad,wrad,angmesh,emd_a,emd_b,k,true);
-    double BBa=similarity_quadrature(rad,wrad,angmesh,emd_b,emd_b,k,true);
-    ret(1+k,1)=sqrt(AAa+BBa-2*ABa);
-  }
+      //      printf("k = %i, ave = %i: AA = %e, BB = %e, AB = %e\n",k,ave,ret(1+k,0,ave),ret(1+k,1,ave),ret(1+k,2,ave));
+    }
 
   if(verbose) {
     printf("Similarity moments computed in %s.\n",t.elapsed().c_str());
@@ -187,3 +176,17 @@ arma::mat emd_similarity(const BasisSet & basis_a, const arma::mat & P_a, const 
   return ret;
 }
 
+arma::cube emd_similarity(const arma::cube & emd, int Nela, int Nelb) {
+  // Compute shape function overlap                                                                                                                                                                              
+  arma::cube sh(4,4,2);
+  sh.zeros();
+  for(size_t s=0;s<sh.n_slices;s++)
+    for(size_t k=0;k<sh.n_rows;k++) {
+      sh(k,0,s)=emd(k,0,s)/(Nela*Nela);
+      sh(k,1,s)=emd(k,1,s)/(Nelb*Nelb);
+      sh(k,2,s)=emd(k,2,s)/(Nela*Nelb);
+      sh(k,3,s)=sqrt(sh(k,0,s) + sh(k,1,s) - 2*sh(k,2,s));
+    }
+
+  return sh;
+}
