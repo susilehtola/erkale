89a0ec2b359c98a497e9e6f0dd6445ed16de07be
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2014-02-14 12:56:12 +0000

    Can now load reference Hirshfeld densities from checkpoints.

--- src/hirshfeldi.cpp
+++ src/hirshfeldi.cpp
@@ -14,13 +14,22 @@
  * of the License, or (at your option) any later version.
  */
 
+#include "elements.h"
+#include "checkpoint.h"
 #include "hirshfeldi.h"
 #include "dftgrid.h"
 #include "guess.h"
 #include "elements.h"
 #include "timer.h"
 
-HirshfeldI::HirshfeldI(const BasisSet & basis, const arma::mat & P, std::string method, double tol, double drv, int dqmax, bool verbose) {
+HirshfeldI::HirshfeldI() {
+  dr=0.0;
+}
+
+HirshfeldI::~HirshfeldI() {
+}
+
+void HirshfeldI::compute(const BasisSet & basis, const arma::mat & P, std::string method, double tol, double drv, int dqmax, bool verbose) {
   // Store grid spacing
   dr=drv;
 
@@ -67,7 +76,7 @@ HirshfeldI::HirshfeldI(const BasisSet & basis, const arma::mat & P, std::string 
       std::vector<size_t> shellidx;
       // Don't drop polarization shells, but do occupation smearing
       atomic_guess(basis,idnuc[i][0],method,shellidx,atbas,atE,atP,false,true,dq);
-      
+
       // Construct atom
       HirshfeldAtom at(atbas,atP,dr);
       // get the density
@@ -90,6 +99,93 @@ HirshfeldI::HirshfeldI(const BasisSet & basis, const arma::mat & P, std::string 
     t.set();
   }
 
+  // Perform solution
+  solve(basis,P,tol,verbose);
+
+  if(verbose)
+    printf("Iterative Hirshfeld decomposition computed in %s.\n\n",ttot.elapsed().c_str());
+
+}
+
+void HirshfeldI::compute_load(const BasisSet & basis, const arma::mat & P, double tol, double drv, int dqmax, bool verbose) {
+  // Store grid spacing
+  dr=drv;
+
+  Timer ttot;
+
+  // Store atomic centers.
+  cen.resize(basis.get_Nnuc());
+  for(size_t i=0;i<cen.size();i++)
+    cen[i]=basis.get_nucleus(i).r;
+
+  // Reserve memory for atomic densities
+  atoms.resize(basis.get_Nnuc());
+  atQ.resize(basis.get_Nnuc());
+  for(size_t i=0;i<cen.size();i++) {
+    atoms[i].resize(2*dqmax+1);
+    atQ[i].assign(2*dqmax+1,0.0);
+  }
+  
+  // Get list of nuclei
+  std::vector<nucleus_t> nuc=basis.get_nuclei();
+  // Get list of elements in system
+  std::vector< std::vector<size_t> > Zv(maxZ+1);
+  for(size_t i=0;i<nuc.size();i++) {
+    if(nuc[i].bsse)
+      continue;
+    Zv[nuc[i].Z].push_back(i);
+  }
+
+  if(verbose)
+    printf("\t%4s %2s %6s %4s\n","atom","el","charge","time");
+
+  // Loop over elements
+  for(int Z=0;Z<(int) Zv.size();Z++)
+    if(Zv[Z].size()) {
+      // Loop over electron count
+      for(int nel=std::max(1,Z-dqmax);nel<=Z+dqmax;nel++) {
+	Timer tatq;
+
+	// Charge difference of system is
+	int dq=Z-nel;
+	
+	// Load checkpoint
+	char chkname[80];
+	sprintf(chkname,"%s_%i.chk",element_symbols[Z].c_str(),dq);
+	Checkpoint chkpt(chkname,false);
+	
+	// Load basis set and density matrix
+	BasisSet bas;
+	chkpt.read(bas);
+	
+	arma::mat Pat;
+	chkpt.read("P",Pat);
+
+	// Construct atom
+	HirshfeldAtom at(bas,Pat);
+	// and store it
+	for(size_t j=0;j<Zv[Z].size();j++) {
+	  atoms[Zv[Z][j]][dqmax-dq]=at.get_rho();
+	  atQ[Zv[Z][j]][dqmax-dq]=nel;
+	}
+
+	if(verbose) {
+	  printf("\t%4i %-2s %6i %s\n", (int) Zv[Z][0]+1, element_symbols[Z].c_str(),dq,tatq.elapsed().c_str());
+	  fflush(stdout);
+	}
+      }
+    }
+  
+  // Perform solution
+  solve(basis,P,tol,verbose);
+  
+  if(verbose)
+    printf("Iterative Hirshfeld decomposition computed in %s.\n\n",ttot.elapsed().c_str());
+}
+
+void HirshfeldI::solve(const BasisSet & basis, const arma::mat & P, double tol, bool verbose) {
+  Timer t;
+
   // Starting guess: neutral species
   arma::vec q(cen.size());
   for(size_t i=0;i<cen.size();i++) {
@@ -100,9 +196,9 @@ HirshfeldI::HirshfeldI(const BasisSet & basis, const arma::mat & P, std::string 
       q[i]=nuc.Z;
   }
   ISA=get(q);
-
+  
   //  arma::trans(q).print("Starting guess");
-
+  
   // First iteration
   if(verbose) {
     printf("First iteration\n");
@@ -117,11 +213,7 @@ HirshfeldI::HirshfeldI(const BasisSet & basis, const arma::mat & P, std::string 
   iterate(basis,P,q,tol,verbose);
   if(verbose) {
     printf("Converged in %s.\n",t.elapsed().c_str());
-    printf("Iterative Hirshfeld decomposition computed in %s.\n\n",ttot.elapsed().c_str());
   }
-}
-
-HirshfeldI::~HirshfeldI() {
 }
 
 void HirshfeldI::iterate(const BasisSet & basis, const arma::mat & P, arma::vec & q, double tol, bool verbose) {
