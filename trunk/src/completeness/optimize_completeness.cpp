38127ef5615160a8acde9b09a8fcce3c7ea22336
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2014-04-21 21:23:36 +0000

    Use Fletcher-Reeves conjugate gradients in completeness optimization. Exact linesearch for maximum width of plateau.

--- trunk/src/completeness/optimize_completeness.cpp
+++ trunk/src/completeness/optimize_completeness.cpp
@@ -85,7 +85,7 @@ arma::vec completeness_profile(const gsl_vector * x, void * params) {
 
   // and its half inverse matrix
   arma::mat Sinvh=BasOrth(Suv,false);
- 
+
   // Get overlap of primitives with scanning terms
   arma::mat amu=overlap(par->scanexp,z,par->am);
 
@@ -151,7 +151,61 @@ double compl_mog(const gsl_vector * x, void * params) {
   return phi;
 }
 
-arma::vec optimize_completeness(int am, double min, double max, int Nf, int n, bool verbose, double *mog) {
+void compl_mog_df(const gsl_vector * x, void * params, gsl_vector * g) {
+  // Step size to use. Since we are working in logarithmic space, this
+  // should be always OK.
+  double h=1e-4;
+
+#ifdef _OPENMP
+  int nth=omp_get_max_threads();
+#else
+  int nth=1;
+#endif
+  // Helper vectors for parameters
+  std::vector<gsl_vector *> stack(nth);
+  for(int i=0;i<nth;i++)
+    stack[i]=gsl_vector_alloc(x->size);
+
+  // Compute gradient using central difference
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+  for(size_t ip=0; ip < x->size; ip++) {
+#ifdef _OPENMP
+    int ith=omp_get_thread_num();
+#else
+    int ith=0;
+#endif
+
+    // Initialize helper vector
+    gsl_vector_memcpy(stack[ith],x);
+
+    // Original parameter value
+    double x0=gsl_vector_get(x,ip);
+
+    // RH value
+    gsl_vector_set(stack[ith],ip,x0+h);
+    double rh=compl_mog(stack[ith],params);
+
+    // LH value
+    gsl_vector_set(stack[ith],ip,x0-h);
+    double lh=compl_mog(stack[ith],params);
+
+    // Gradient value is
+    gsl_vector_set(g,ip, (rh-lh)/(2.0*h));
+  }
+
+  // Free memory
+  for(int i=0;i<nth;i++)
+    gsl_vector_free(stack[i]);
+}
+
+void compl_mog_fdf(const gsl_vector * x, void * params, double * f, gsl_vector * g) {
+  *f=compl_mog(x,params);
+  compl_mog_df(x,params,g);
+}
+
+arma::vec optimize_completeness_simplex(int am, double min, double max, int Nf, int n, bool verbose, double *mog) {
   // Optimized exponents
   arma::vec exps;
 
@@ -186,7 +240,7 @@ arma::vec optimize_completeness(int am, double min, double max, int Nf, int n, b
   } else {
     // Time minimization
     Timer tmin;
-    
+
     // Optimized profile will be always symmetric around the midpoint,
     // so we can use this to reduce the amount of degrees of freedom in
     // the optimization. For even amount of exponents, the mid exponent
@@ -231,8 +285,8 @@ arma::vec optimize_completeness(int am, double min, double max, int Nf, int n, b
 
       printf("iter ");
       char num[80];
-      for(int i=0;i<Nf;i++) {
-	sprintf(num,"lg exp%i",i+1);
+      for(int i=0;i<Ndof;i++) {
+	sprintf(num,"lg par%i",i+1);
 	printf("%9s ",num);
       }
       printf(" %12s  %12s\n","tau","size");
@@ -271,18 +325,160 @@ arma::vec optimize_completeness(int am, double min, double max, int Nf, int n, b
 
     // The optimized exponents in descending order
     exps=arma::sort(get_exponents(s->x,&pars),1);
-  
+
     gsl_vector_free(x);
     gsl_vector_free(ss);
     gsl_multimin_fminimizer_free (s);
-  
+
     if(verbose)
       printf("\nMinimization completed in %s.\n",tmin.elapsed().c_str());
   }
 
   // Move starting point from 0 to start
   exps*=std::pow(10.0,min+len/2.0);
-  
+
+  return exps;
+}
+
+arma::vec optimize_completeness(int am, double min, double max, int Nf, int n, bool verbose, double *mog) {
+  // Optimized exponents
+  arma::vec exps;
+
+  // Length of interval is
+  double len=max-min;
+
+  // Parameters for the optimization.
+  completeness_scan_t pars;
+  // Angular momentum
+  pars.am=am;
+  // Moment to optimize
+  pars.n=n;
+  // Scanning exponents
+  pars.scanexp=get_scanning_exponents(-len/2.0,len/2.0,50*Nf+1);
+  // Odd amount of exponents?
+  pars.odd=Nf%2;
+
+  if(Nf<1) {
+    throw std::runtime_error("Cannot completeness-optimize less than one primitive.\n");
+
+  } else if(Nf==1) {
+    // Only single exponent at exp(0) = 1.0.
+    gsl_vector x;
+    x.size=0;
+
+    // Get exponent
+    exps=get_exponents(&x,&pars);
+    // and the mog
+    if(mog!=NULL)
+      *mog=compl_mog(&x,(void *) &pars);
+
+  } else {
+    // Time minimization
+    Timer tmin;
+
+    // Optimized profile will be always symmetric around the midpoint,
+    // so we can use this to reduce the amount of degrees of freedom in
+    // the optimization. For even amount of exponents, the mid exponent
+    // is pinned to the midway of the interval.
+    int Ndof=Nf/2;
+
+    // Maximum number of iterations
+    size_t maxiter = 10000;
+
+    // Use Fletcher-Reeves gradient, which works here better than Polak-Ribiere or BFGS.
+    const gsl_multimin_fdfminimizer_type *T = gsl_multimin_fdfminimizer_conjugate_fr;
+    gsl_multimin_fdfminimizer *s = NULL;
+    gsl_multimin_function_fdf minfunc;
+
+    size_t iter = 0;
+    int status;
+
+    /* Starting point */
+    gsl_vector *x = gsl_vector_alloc (Ndof);
+    for(int i=0;i<Ndof;i++)
+      gsl_vector_set(x,i,log(10.0)*((i+0.5)*len/(2.0*Ndof)));
+
+    /* Set initial step sizes to unity */
+    gsl_vector *ss = gsl_vector_alloc (Ndof);
+    gsl_vector_set_all (ss, 1.0);
+
+    /* Initialize method and iterate */
+    minfunc.n = Ndof;
+    minfunc.f = compl_mog;
+    minfunc.df = compl_mog_df;
+    minfunc.fdf = compl_mog_fdf;
+    minfunc.params = (void *) &pars;
+
+    s = gsl_multimin_fdfminimizer_alloc (T, Ndof);
+
+    // Initial step size is 0.01, and the line minimization parameter is 1e-4
+    gsl_multimin_fdfminimizer_set (s, &minfunc, x, 0.01, 1e-4);
+
+    // Progress timer
+    Timer t;
+
+    // Legend
+    if(verbose) {
+      printf("Optimizing tau_%i for a=[%.3f ... %.3f] of %c shell with %i exponents.\n\n",n,min,max,shell_types[am],Nf);
+
+      printf("iter ");
+      char num[80];
+      for(int i=0;i<Ndof;i++) {
+	sprintf(num,"lg par%i",i+1);
+	printf("%9s ",num);
+      }
+      printf(" %12s  %12s\n","tau","grad norm");
+    }
+
+    do
+      {
+	iter++;
+	status = gsl_multimin_fdfminimizer_iterate(s);
+
+	if (status)
+	  break;
+
+	status = gsl_multimin_test_gradient (s->gradient, 1e-8);
+
+	if (status == GSL_SUCCESS && verbose)
+	  printf ("converged to minimum at\n");
+
+	if(verbose) {
+	  t.set();
+	  printf("%4u ",(unsigned int) iter);
+	  for(int i=0;i<Ndof;i++)
+	    // Convert to 10-base logarithm
+	    printf("% 9.5f ",log10(M_E)*gsl_vector_get(s->x,i));
+	  printf(" %e  %e\n",pow(s->f,1.0/n),gsl_blas_dnrm2(s->gradient));
+
+	  // print_gradient(s->x,(void *) &pars);
+	}
+      }
+    while (status == GSL_CONTINUE && iter < maxiter);
+
+    /*
+    if (status != GSL_SUCCESS)
+      printf ("encountered error \"%s\"\n",gsl_strerror(status));
+    */
+
+    // Save mog
+    if(mog!=NULL)
+      *mog=pow(s->f,1.0/n);
+
+    // The optimized exponents in descending order
+    exps=arma::sort(get_exponents(s->x,&pars),1);
+
+    gsl_vector_free(x);
+    gsl_vector_free(ss);
+    gsl_multimin_fdfminimizer_free (s);
+
+    if(verbose)
+      printf("\nMinimization completed in %s.\n",tmin.elapsed().c_str());
+  }
+
+  // Move starting point from 0 to start
+  exps*=std::pow(10.0,min+len/2.0);
+
   return exps;
 }
 
@@ -301,90 +497,82 @@ arma::vec maxwidth_exps(int am, double tol, int nexp, double *width, int nval) {
   }
 
   if(tol<MINTAU) {
-    //    printf("Renormalized CO tolerance to 1e-5.\n");
+    printf("Renormalized CO tolerance to %e.\n",MINTAU);
     tol=MINTAU;
   }
 
-  // Left value - vanishing plateau, i.e. too large completeness value
+  // Left value - vanishing plateau width, i.e. tau too small
   double left=0.0;
+  double lval=0.0;
 
-  // Right value - pretty big plateau, i.e. too small completeness value
+  // Right value - pretty big plateau, i.e. tau too big
   double right=0.5*nexp;
   double rval;
 
   // Check that right value is OK
   arma::vec rexps=optimize_completeness(am,0.0,right,nexp,nval,false,&rval);
   while(rval<tol) {
+    // Must have tau too big here
     left=right;
     right*=2.0;
     rexps=optimize_completeness(am,0.0,right,nexp,nval,false,&rval);
   }
 
-  // Left value
+  // Check for unitialized left value
   if(left==0.0) {
-    double lval=rval;
+    left=right;
+    lval=rval;
     arma::vec lexps;
     while(lval>tol) {
+      // Must have tau too small here
       left/=2.0;
       lexps=optimize_completeness(am,0.0,left,nexp,nval,false,&lval);
     }
   }
 
-#ifndef _OPENMP
-  int nth=1;
-#else
-  int nth=omp_get_max_threads();
-#endif
-  
-  // Worker stack  
-  std::vector<arma::vec> expstack(nth);
-  // Widths
-  std::vector<double> wstack(nth);
-  // Tolerances
-  std::vector<double> tstack(nth);
-  
-  do {
-    // Set widths
-    for(int i=0;i<nth;i++)
-      wstack[i]=left + (i+1)*(right-left)/(nth+1);
+  // Refine until in linear regime
+  size_t ii;
+  const size_t iimax=100;
+  for(ii=0;ii<iimax;ii++) {
+    // Compute midpoint value
+    double middle=(right+left)/2.0;
+    double mval;
+    arma::vec mexps=optimize_completeness(am,0.0,middle,nexp,nval,false,&mval);
 
-    // Get exponents
-#ifdef _OPENMP
-#pragma omp parallel
-#endif
-    {
-      // Get thread number
-#ifdef _OPENMP
-      int ith=omp_get_thread_num();
-#else
-      int ith=0;
-#endif
-      // Get exponents
-      expstack[ith]=optimize_completeness(am,0.0,wstack[ith],nexp,nval,false,&tstack[ith]);
-    }
-    
-    // Figure out how to move ends
-    for(int i=0;i<nth;i++) {
-      if(tstack[i]>tol && right>wstack[i])
-	right=wstack[i];
-      else if(tstack[i]<tol && left<wstack[i])
-	left=wstack[i];
-    }
-  } while(right-left>1e-7);
+    // Check for linear regime: predicted value is
+    double mpred=lval + (middle-left)/(right-left)*(rval-lval);
+    //    printf("mpred accuracy: %e\n",fabs((mpred-mval)/mval));
 
-  // Find value closest to tol
-  double min=DBL_MAX;
-  int mini=-1;
-  for(int i=0;i<nth;i++)
-    if(fabs(tstack[i]-tol)<min) {
-      min=fabs(tstack[i]-tol);
-      mini=i;
+    // and we are converged if the correction term is O(eps)
+    bool converged = (fabs((mpred-mval)) <= sqrt(DBL_EPSILON)*fabs(mval));
+
+    // Update limits
+    if(mval<tol) {
+      left=middle;
+      lval=mval;
+    } else if(mval>tol) {
+      right=middle;
+      rval=mval;
     }
-  
-  // Set width
-  *width=wstack[mini];
-  
-  return expstack[mini];
+
+    // Stop if converged
+    if(converged)
+      break;
+  }
+
+  if(ii==iimax)
+    throw std::runtime_error("Error finding limits in maxwidth_exps.\n");
+
+  // Interpolate to the exact value
+  *width=left + (tol-lval)/(rval-lval)*(right-left);
+
+  // Exponents and realized value of tau are
+  double tau;
+  arma::vec exps=optimize_completeness(am,0.0,*width,nexp,nval,false,&tau);
+
+  //  printf("Interpolation from w = %e .. %e with tau = %e .. %e yielded w = %e, tau = %e.\n",left,right,lval,rval,*width,tau);
+
+  return exps;
 }
 
 
--- trunk/src/completeness/optimize_completeness.h
+++ trunk/src/completeness/optimize_completeness.h
@@ -18,7 +18,7 @@
 #define ERKALE_OPTCOMP
 
 /// Minimum allowed value of deviation from completeness (for numerical stability)
-#define MINTAU pow(10.0,-4.8)
+#define MINTAU pow(10.0,-4.5)
 
 #include "../global.h"
 #include <armadillo>
@@ -53,14 +53,29 @@ arma::vec completeness_profile(const gsl_vector * x, void * params);
 /// Evaluate measure of goodness
 double compl_mog(const gsl_vector * x, void * params);
 
+/// Evaluate gradient of measure of goodness
+void compl_mog_df(const gsl_vector * x, void * params, gsl_vector * g);
+
+/// Evaluate gradient and measure of goodness
+void compl_mog_fdf(const gsl_vector * x, void * params, double * f, gsl_vector * df);
+
 /**
  * Optimize completeness profile for angular momentum am in exponent
  * range from 10^{min} to 10^{max}. n gives the moment to optimize for
  * (1 for maximal area, 2 for minimal rms deviation from unity).
  *
- * This routine uses the Nead-Miller algorithm.
+ * This routine uses Fletcher-Reeves conjugate gradients.
  */
 arma::vec optimize_completeness(int am, double min, double max, int Nf, int n=1, bool verbose=true, double *mog=NULL);
+
+/**
+ * Optimize completeness profile for angular momentum am in exponent
+ * range from 10^{min} to 10^{max}. n gives the moment to optimize for
+ * (1 for maximal area, 2 for minimal rms deviation from unity).
+ *
+ * This is the old version of the routine, which uses the Nead-Miller algorithm.
+ */
+arma::vec optimize_completeness_simplex(int am, double min, double max, int Nf, int n=1, bool verbose=true, double *mog=NULL);
 
 /// Calculate maximum width to obtain tolerance with given amount of exponents
 double maxwidth(int am, double tol, int nexp, int n=1);
