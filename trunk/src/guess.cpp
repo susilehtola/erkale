d9ce03b6e69d6cd9aca966006db4ccab66fb7aee
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2015-05-20 05:35:16 +0000

    Forgot a debug output.

--- trunk/src/guess.cpp
+++ trunk/src/guess.cpp
@@ -80,8 +80,6 @@ void atomic_guess(const BasisSet & basis, size_t inuc, const std::string & metho
   set.set_double("DeltaPmax",1e-5);
   set.set_double("DeltaPrms",1e-6);
 
-  set.print();
-  
   // Construct the basis set
   atbas=BasisSet(1,set);
   // Add the nucleus
