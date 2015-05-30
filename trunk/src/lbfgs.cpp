dcecec164489513b34f5bc2ad12cb76f681cc214
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2015-05-30 06:27:23 +0000

    BFGS for OO optimization.

--- trunk/src/lbfgs.cpp
+++ trunk/src/lbfgs.cpp
@@ -9,7 +9,7 @@ LBFGS::~LBFGS() {
 void LBFGS::update(const arma::vec & x, const arma::vec & g) {
   xk.push_back(x);
   gk.push_back(g);
-
+  
   if(xk.size()>nmax) {
     xk.erase(xk.begin());
     gk.erase(gk.begin());
@@ -17,54 +17,57 @@ void LBFGS::update(const arma::vec & x, const arma::vec & g) {
 }
 
 arma::vec LBFGS::diagonal_hessian(const arma::vec & q) const {
-  arma::vec s=xk[xk.size()-1]-xk[xk.size()-2];
-  arma::vec y=gk[gk.size()-1]-gk[gk.size()-2];
+  if(xk.size()>=2) {
+    arma::vec s=xk[xk.size()-1]-xk[xk.size()-2];
+    arma::vec y=gk[gk.size()-1]-gk[gk.size()-2];
+    
+    return arma::dot(s,y)/arma::dot(y,y)*q;
 
-  return arma::dot(s,y)/arma::dot(y,y)*q;
+  } else
+    // Unit diagonal Hessian
+    return q;
 }
 
 arma::vec LBFGS::solve() const {
   // Algorithm 9.1 in Nocedal's book
   size_t k=gk.size()-1;
-  arma::vec q=gk[k];
+  arma::vec q(gk[k]);
 
-  // Sanity check
-  if(k==0)
-    return q;
-  
-  // Compute differences
   std::vector<arma::vec> sk(k);
   for(size_t i=0;i<k;i++)
     sk[i]=xk[i+1]-xk[i];
   std::vector<arma::vec> yk(k);
   for(size_t i=0;i<k;i++)
     yk[i]=gk[i+1]-gk[i];
-
+  
   // Alpha_i
   std::vector<double> alphai(k);
   
   // First part
-  for(size_t i=k-1;i<gk.size();i--) {
-    // Rho_i
-    double rhoi=1.0/arma::dot(yk[i],sk[i]);
+  for(size_t i=k-1;i<k;i--) {
     // Alpha_i
-    alphai[i]=rhoi*arma::dot(sk[i],q);
+    alphai[i]=arma::dot(sk[i],q)/arma::dot(yk[i],sk[i]);
     // Update q
     q-=alphai[i]*yk[i];
   }
-
+  
   // Apply diagonal Hessian
   arma::vec r(diagonal_hessian(q));
-
+  
   // Second part
   for(size_t i=0;i<k;i++) {
-    // Rho_i
-    double rhoi=1.0/arma::dot(yk[i],sk[i]);
     // Beta
-    double beta=rhoi*arma::dot(yk[i],r);
+    double beta=arma::dot(yk[i],r)/arma::dot(yk[i],sk[i]);
     // Update r
     r+=sk[i]*(alphai[i]-beta);
   }
 
   return r;
 }
+
+void LBFGS::clear() {
+  xk.clear();
+  gk.clear();
+}
+
+
