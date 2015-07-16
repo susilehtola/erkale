535743f307772108822ad6b0d0ba0f64f5293518
jussilehtola <jussilehtola@d6feeb75-070b-80ad-bd74-06a9172287bc>
2015-07-16 23:10:57 +0000

    Add missing license boilerplates.

--- trunk/src/lbfgs.cpp
+++ trunk/src/lbfgs.cpp
@@ -1,3 +1,21 @@
+/*
+ *                This source code is part of
+ *
+ *                     E  R  K  A  L  E
+ *                             -
+ *                       HF/DFT from Hel
+ *
+ * Copyright © 2015 The Regents of the University of California
+ * All Rights Reserved
+ *
+ * Written by Susi Lehtola, Lawrence Berkeley National Laboratory
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
 #include "lbfgs.h"
 
 LBFGS::LBFGS(size_t nmax_) : nmax(nmax_) {
