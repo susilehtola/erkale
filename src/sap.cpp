ea041ae84a92a92da3b837ed9affd2a9372f7a36
Susi Lehtola <susi.lehtola@gmail.com>
2018-11-07 11:59:24 +0200

    Add license boilerplates.

--- src/sap.cpp
+++ src/sap.cpp
@@ -1,3 +1,19 @@
+/*
+ *                This source code is part of
+ *
+ *                     E  R  K  A  L  E
+ *                             -
+ *                       DFT from Hel
+ *
+ * Written by Susi Lehtola, 2018
+ * Copyright (c) 2018, Susi Lehtola
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
 #include "sap.h"
 #include "elements.h"
 
