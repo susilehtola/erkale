614c0dbdfc3d4d418324f84641b96e9469516344
Susi Lehtola <susi.lehtola@gmail.com>
2020-11-20 10:46:25 -0500

    Fixes to dumpxc

--- src/contrib/dumpxc.cpp
+++ src/contrib/dumpxc.cpp
@@ -56,9 +56,11 @@ int main(int argc, char **argv) {
 
   // Parse settings
   settings.add_string("LoadChk","Checkpoint file to load density from","erkale.chk");
-  settings.add_string("Method","Functional to dump data for","mgga_x_tpss");
+  settings.add_string("Method","Functional to dump data for","mgga_x_br89");
   settings.add_double("GridTol","DFT grid tolerance to use",1e-3);
   settings.add_string("DFTGrid","DFT grid to use","Auto");
+  settings.add_string("DFTXpars","exhange functional parameters","");
+  settings.add_string("DFTCpars","correlation fucntional parameters","");
   if(argc==2)
     settings.parse(argv[1]);
   else printf("Using default settings.\n\n");
@@ -80,45 +82,42 @@ int main(int argc, char **argv) {
   chkpt.read("Restricted",restr);
 
   // Functional id
-  int x_func, c_func;
-  parse_xc_func(x_func,c_func,settings.get_string("Method"));
+  dft_t dft;
+  parse_xc_func(dft.x_func,dft.c_func,settings.get_string("Method"));
 
   // DFT grid, verbose operation
   DFTGrid grid(&basis,true);
 
-  // Adaptive grid?
-  bool adaptive;
-  int nrad, lmax;
-  double gridtol;
-
   // Determine grid
-  std::string dftgrid=settings.get_string("DFTGrid");
-  if(stricmp(dftgrid,"Auto")==0) {
-    adaptive=true;
-    gridtol=settings.get_double("GridTol");
+  std::string dftgrid(settings.get_string("DFTGrid"));
+  std::string tolkw("GridTol");
+  if(stricmp(dftgrid,"Auto")!=0) {
+    parse_grid(dft, settings.get_string("DFTGrid"), "DFT");
   } else {
-    adaptive=false;
-    std::vector<std::string> gridsize=splitline(dftgrid);
-    if(gridsize.size()!=2)
-      throw std::runtime_error("Error determining grid size.\n");
-    nrad=readint(gridsize[0]);
-    lmax=readint(gridsize[1]);
+    dft.adaptive=true;
+    dft.gridtol=settings.get_double(tolkw);
   }
 
   if(restr) {
-    if(adaptive)
-      grid.construct(P,gridtol,x_func,c_func);
-    else
-      grid.construct(nrad,lmax,x_func,c_func,false);
+    if(dft.adaptive) {
+      // The adaptive routine does everything automatically
+      grid.construct(P,dft.gridtol,dft.x_func,dft.c_func);
+    } else{
+      grid.construct(dft.nrad,dft.lmax,dft.x_func,dft.c_func,false);
+      // Need to compute the xc functional to make sure all fields are populated
+      arma::mat F;
+      double Exc, Nel;
+      grid.eval_Fxc(dft.x_func,dft.c_func,P,F,Exc,Nel);
+    }
 
     grid.print_density(P);
-    if(x_func) {
-      grid.print_potential(x_func,P/2.0,P/2.0,"Vx.dat");
-      grid.check_potential(x_func,P/2.0,P/2.0,"Vx_nan.dat");
+    if(dft.x_func) {
+      grid.print_potential(dft.x_func,P/2.0,P/2.0,"Vx.dat");
+      grid.check_potential(dft.x_func,P/2.0,P/2.0,"Vx_nan.dat");
     }
-    if(c_func) {
-      grid.print_potential(c_func,P/2.0,P/2.0,"Vc.dat");
-      grid.check_potential(c_func,P/2.0,P/2.0,"Vc_nan.dat");
+    if(dft.c_func) {
+      grid.print_potential(dft.c_func,P/2.0,P/2.0,"Vc.dat");
+      grid.check_potential(dft.c_func,P/2.0,P/2.0,"Vc_nan.dat");
     }
 
   } else {
@@ -126,19 +125,24 @@ int main(int argc, char **argv) {
     chkpt.read("Pa",Pa);
     chkpt.read("Pb",Pb);
 
-    if(adaptive)
-      grid.construct(Pa,Pb,gridtol,x_func,c_func);
-    else
-      grid.construct(nrad,lmax,x_func,c_func,false);
+    if(dft.adaptive) {
+      grid.construct(Pa,Pb,dft.gridtol,dft.x_func,dft.c_func);
+    } else {
+      grid.construct(dft.nrad,dft.lmax,dft.x_func,dft.c_func,false);
+      // Need to compute the xc functional to make sure all fields are populated
+      arma::mat Fa, Fb;
+      double Exc, Nel;
+      grid.eval_Fxc(dft.x_func,dft.c_func,Pa,Pb,Fa,Fb,Exc,Nel);
+    }
 
     grid.print_density(Pa,Pb);
-    if(x_func) {
-      grid.print_potential(x_func,Pa,Pb,"Vx.dat");
-      grid.check_potential(x_func,Pa,Pb,"Vx_nan.dat");
+    if(dft.x_func) {
+      grid.print_potential(dft.x_func,Pa,Pb,"Vx.dat");
+      grid.check_potential(dft.x_func,Pa,Pb,"Vx_nan.dat");
     }
-    if(c_func) {
-      grid.print_potential(c_func,Pa,Pb,"Vc.dat");
-      grid.check_potential(c_func,Pa,Pb,"Vc_nan.dat");
+    if(dft.c_func) {
+      grid.print_potential(dft.c_func,Pa,Pb,"Vc.dat");
+      grid.check_potential(dft.c_func,Pa,Pb,"Vc_nan.dat");
     }
   }
 
