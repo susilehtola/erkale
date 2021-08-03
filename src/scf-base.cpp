susilehtola/erkale

commit 676f71d1246a065ba9750a1d64df6670f9a58747

S^2 expectation value was not computed correctly in PZ-SIC.

src/scf-base.cpp
@@ -2873,10 +2873,10 @@ void calculate(const BasisSet & basis, bool force) {
       population_analysis(basis,sol.Pa,sol.Pb);
 
       if(!settings.get_string("Occupancies").size()) {
-	arma::mat Ca(sol.Ca.cols(0,Nel_alpha-1));
-	arma::mat Cb;
+	arma::cx_mat Ca(sol.cCa.cols(0,Nel_alpha-1));
+	arma::cx_mat Cb;
 	if(Nel_beta)
-	  Cb=sol.Cb.cols(0,Nel_beta-1);
+	  Cb=sol.cCb.cols(0,Nel_beta-1);
 	double S2(spin_S2(basis,Ca,Cb));
 	printf("\nThe spin expectation value <S^2> is %f\n",S2);
       } else {

src/properties.cpp
@@ -842,6 +842,25 @@ double spin_S2(const BasisSet & basis, const arma::mat & Ca, const arma::mat & C
   return S2;
 }
 
+double spin_S2(const BasisSet & basis, const arma::cx_mat & Ca, const arma::cx_mat & Cb) {
+  arma::uword Na=Ca.n_cols;
+  arma::uword Nb=Cb.n_cols;
+
+  arma::mat S(basis.overlap());
+  double Sz=(Na-Nb)/2.0;
+
+  arma::cx_mat Sab;
+  if(Nb)
+    Sab=arma::trans(Ca)*S*Cb;
+
+  double S2=Sz*(Sz+1) + Nb;
+  for(arma::uword i=0;i<Na;i++)
+    for(arma::uword j=0;j<Nb;j++)
+      S2-=std::norm(Sab(i,j));
+
+  return S2;
+}
+
 double darwin_1e(const BasisSet & basis, const arma::mat & P) {
   // Energy
   double E=0.0;

src/properties.h
@@ -161,6 +161,8 @@ void population_analysis(const BasisSet & basis, const arma::mat & Pa, const arm
 
 /// Calculate S^2 spin expectation value, given sets of occupied orbitals
 double spin_S2(const BasisSet & basis, const arma::mat & Ca, const arma::mat & Cb);
+/// Calculate S^2 spin expectation value, given sets of occupied orbitals
+double spin_S2(const BasisSet & basis, const arma::cx_mat & Ca, const arma::cx_mat & Cb);
 
 /**
  * Get Darwin one-electron term.

commit 8e3b37f0c664d37382a8e023671f87e7a4cd0a69

Fix memory issue introduced by commit 676f71d1246a065ba9750a1d64df6670f9a58747

src/scf-base.cpp
@@ -2616,8 +2616,22 @@ void calculate(const BasisSet & basis, bool force) {
 	chkpt.write(sol.en);
 	chkpt.write("Converged",1);
 
+        // Run population analysis
+        if(verbose) {
+          population_analysis(basis,sol.Pa,sol.Pb);
+          if(!settings.get_string("Occupancies").size()) {
+            arma::mat Ca(sol.Ca.cols(0,Nel_alpha-1));
+            arma::mat Cb;
+            if(Nel_beta)
+              Cb=sol.Cb.cols(0,Nel_beta-1);
+            double S2(spin_S2(basis,Ca,Cb));
+            printf("\nThe spin expectation value <S^2> is %f\n",S2);
+          } else {
+            printf("\nNot calculating spin expectation value since occupancies is settings.\n");
+          }
+        }
 
-      } else {
+      } else if(pz) {
 	chkpt.write("Converged",0);
 
 	// The localizing matrix
@@ -2861,24 +2875,24 @@ void calculate(const BasisSet & basis, bool force) {
 	  stab.set_params(true,pzimag!=0,pzov,pzoo);
 	  stab.check();
 	}
-      }
 
-      // Calculation has converged.
-      chkpt.write("Converged",1);
-    }
+        // Calculation has converged.
+        chkpt.write("Converged",1);
 
-    if(verbose) {
-      population_analysis(basis,sol.Pa,sol.Pb);
-
-      if(!settings.get_string("Occupancies").size()) {
-	arma::cx_mat Ca(sol.cCa.cols(0,Nel_alpha-1));
-	arma::cx_mat Cb;
-	if(Nel_beta)
-	  Cb=sol.cCb.cols(0,Nel_beta-1);
-	double S2(spin_S2(basis,Ca,Cb));
-	printf("\nThe spin expectation value <S^2> is %f\n",S2);
-      } else {
-	printf("\nNot calculating spin expectation value since occupancies is settings.\n");
+        // Run population analysis
+        if(verbose) {
+          population_analysis(basis,sol.Pa,sol.Pb);
+          if(!settings.get_string("Occupancies").size()) {
+            arma::cx_mat Ca(sol.cCa.cols(0,Nel_alpha-1));
+            arma::cx_mat Cb;
+            if(Nel_beta)
+              Cb=sol.cCb.cols(0,Nel_beta-1);
+            double S2(spin_S2(basis,Ca,Cb));
+            printf("\nThe spin expectation value <S^2> is %f\n",S2);
+          } else {
+            printf("\nNot calculating spin expectation value since occupancies is settings.\n");
+          }
+        }
       }
     }
   }